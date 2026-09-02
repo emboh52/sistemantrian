26d99d29c96c3e0546359518536185c0e473f360
unknown <emboh52@gmail.com>
2026-09-02 09:23:01 +0700

    pwa

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'qantri-pwa-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './register.html',
+  './display.html',
+  './dashboard.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
